6887a13af7bd708fd0f5c7242a232ce9dfca619a
Cristian Duvan Machado Mosquera <62184928+CriistiianDM@users.noreply.github.com>
2023-06-07 19:08:41 -0500

    se termino la vista al produto, voy a juntarlo con el boton de los productos y queda

--- src/components/pages/ViewProduct.jsx
+++ src/components/pages/ViewProduct.jsx
@@ -33,10 +33,19 @@ const characteristics = [
     'VELOCIDAD MÁXIMA 225 km/h',
     'ACELERACIÓN 6,1',
     'AUTONOMÍA (WLTP) 491 km',
-    'DIMENSIONES (LARGO / ANCHO/ ALTO) 4.694 mm / 1.849 mm / 1.443 mm °BATALLA 2.875 mm ',
+    'DIMENSIONES (LARGO / ANCHO/ ALTO) 4.694 mm / 1.849 mm / 1.443 mm',
     'MALETERO TRASERO / DELANTERO 425 litros / 117 litros',
     'BATALLA 2.875 mm',
 ]
+
+const data_color_ext = [
+    '#FFFFFF',
+    '#454343',
+    '#242BC2',
+    '#000000',
+    '#FF0707',
+]
+
 
 /**
  * 
@@ -52,9 +61,14 @@ const ViewProduct = ({
           <nav className="_container_product_view">
             <DestailsPrimary data_img={IMAGES} />
             <Hr name_product="CARACTERISTICAS" />
-            <Characteristics data_product_selected={characteristics} />
+            <Characteristics 
+                data_product_selected={characteristics} 
+            />
             <Hr name_product="COLOR" />
-            <Personalization />
+            <Personalization 
+                 data_color_ext={data_color_ext}
+                 data_color_int={data_color_ext}
+            />
           </nav>
         </>
     )
