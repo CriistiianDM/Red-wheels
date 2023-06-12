f4a7dd6de3f0e8c5a524449b914fdb8171120311
Yi0nn <stefhania.noguera@correounivalle.edu.co>
2023-06-12 18:37:32 -0500

    Prueba 3

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -5,14 +5,11 @@ import Carrusel from '../carrusel/Carrusel';
 import Footer from '../footer/footer';
 import Products from '../products/Products';
 import Navbar from '../menu/Navbar';
-
 // const product = {
 //    price: 100,
 // }
-//prueba
-
 // product.filter((item) => item.price > 100)
-
+//blah blah blah 
 export default function Home() {
   return (
     <>
