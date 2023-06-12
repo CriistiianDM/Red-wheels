8a0c2cd9ffefe7565583a8f62d90a60c20ea8245
Cristian Duvan Machado Mosquera <62184928+CriistiianDM@users.noreply.github.com>
2023-06-12 17:03:17 -0500

    Fixed Styles in login mobile

--- src/components/login/HeaderLogin.jsx
+++ src/components/login/HeaderLogin.jsx
@@ -1,12 +1,19 @@
 import React from "react";
+import { 
+    useNavigate 
+} 
+from "react-router-dom";
+
 
 const HeaderLogin = () => {
-
+     const navigate = useNavigate();
      return (
 
             <>
                 <header className="_container_header_login">
-                    <img src="/assets/icon/logotipo.png" alt="logo" />
+                    <a style={{ cursor: 'pointer' }} onClick={ () => { navigate('/')}}>
+                        <img src="/assets/icon/logotipo.png" alt="logo" />
+                    </a>
                 </header>
             </>
      )
