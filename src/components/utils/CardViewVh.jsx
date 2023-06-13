dfabd8508760f6901940c0790753958218a499f2
Cristian Duvan Machado Mosquera <62184928+CriistiianDM@users.noreply.github.com>
2023-06-12 22:59:21 -0500

    estilos del carrito

---
+++ src/components/utils/CardViewVh.jsx
@@ -0,0 +1,51 @@
+//import libraries
+import React from "react";
+
+
+const CardViewVh = ({
+    data_vehicles,
+    src_img
+}) => {
+
+    const [ img_ , setImg ] = React.useState('/assets/herramientas/car_pd.svg')
+    const [ data_ , setData ] = React.useState([])
+
+    React.useEffect(() => {
+        src_img !== "" && 
+        src_img !== undefined && 
+        src_img !== null && 
+        setImg(src_img) 
+    }, [src_img])
+
+
+    React.useEffect(() => {
+        data_vehicles !== [] &&
+        data_vehicles !== undefined &&
+        data_vehicles !== null &&
+        setData(data_vehicles)
+    }, [data_vehicles])
+
+    return (
+        <> 
+           <section className="_container_hr_vh">
+              <div className="_container_vh_all">
+                <img src={img_} alt="img" />
+                <div className="_absolute_conatiner_hr_all">
+                    {
+                        data_.map((item, index) => {
+                            return (
+                                <a className="_container_hr_vh__item" key={index}>
+                                      <img src={item.img}  alt="_img" />
+                                </a>
+                            )
+                        })
+                    }
+                </div>
+              </div>
+           </section>
+        </>
+    )
+
+}
+
+export default CardViewVh;
--- src/components/utils/NavHerranimientas.jsx
+++ src/components/utils/NavHerranimientas.jsx
@@ -2,34 +2,16 @@
 import React from 'react';
 
 
-const UTILS = [
-    {
-        img: '/assets/herramientas/1.svg',
-        title: 'DIAGNOSTICO'
-    },
-    {
-        img: '/assets/herramientas/2.svg',
-        title: 'MOTOR'
-    },
-    {
-        img: '/assets/herramientas/3.svg',
-        title: 'LIQUIDOS'
-    },
-    {
-        img: '/assets/herramientas/4.svg',
-        title: 'LLANTAS'
-    },
-    {
-        img: '/assets/herramientas/5.svg',
-        title: 'TRANSMISION'
-    },
-    {
-        img: '/assets/herramientas/6.svg',
-        title: 'BATERIAS'
-    }
-]
 
-const NavHerranimientas = () => {
+const NavHerranimientas = ({
+    data_
+}) => {
+
+    const [data, setData] = React.useState([]);
+
+    React.useEffect(() => {
+        setData(data_);
+    }, [data_]);
 
     //mostrando habilidad :3, pero a que costo :(
     return (
@@ -37,15 +19,15 @@ const NavHerranimientas = () => {
             <section className="_container_herramientas">
                 <div className='_sub_container_hrr'>
                     {
-                        UTILS.map((item, index) => {
+                        data.map((item, index) => {
                             
                             return (
-                                <div className="_card_herramientas" key={index}>
+                                <a className="_card_herramientas" key={index}>
                                     <div className="_card_herramientas__img">
                                         <img src={item.img} alt="img" />
                                     </div>
                                     <h1>{item.title}</h1>
-                                </div>
+                                </a>
                             )
                         })
                     }
