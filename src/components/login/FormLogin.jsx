f850fb76c0c227797af152eb34792e4f68c2ace7
Brayan Sanchez <brayan.sanchez.leon@correounivalle.edu.co>
2023-06-14 19:58:28 -0500

    Create state hooks to get the user credentials

--- src/components/login/FormLogin.jsx
+++ src/components/login/FormLogin.jsx
@@ -1,51 +1,56 @@
-import React from "react";
-
+import React, { useState } from "react";
 
 const FormLogin = () => {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [user_type, setUserType] = useState(null);
 
     const handleForm = (e) => {
         e.preventDefault();
     };
 
+    /**
+     * Gets the user credentials.
+     */
+    const handleChange = (e) => {
+        switch (e.target.name) {
+            case "username":
+                setUsername(e.target.value);
+                break;
+            case "password":
+                setPassword(e.target.value);
+                break;
+            case "user_type":
+                setUserType(e.target.value);
+            default:
+        }
+    };
+
     return (
-        <>
-            <div className="_container_form_login">
-                <form onSubmit={handleForm} className="_form_login">
+        <div className="_container_form_login">
+            <form onSubmit={handleForm} className="_form_login">
+                <div className="_img_circle">
+                    <img src="/assets/icon/profile.svg" alt="circle" />
+                </div>
 
-                    <div className="_img_circle">
-                        <img src="/assets/icon/profile.svg" alt="circle" />
-                    </div>
+                <label>Nombre de usuario</label>
+                <input name="username" type="text" placeholder="" onChange={handleChange} />
 
-                    <label>
-                        Nombre de usuario
-                    </label>
-                    <input type="text" placeholder="" />
+                <label>Tipo de usuario</label>
+                <select name="user_type" defaultValue={"0"} onChange={handleChange}>
+                    <option disabled value="0"></option>
+                    <option value="1">Administrador</option>
+                    <option value="2">Estudiante</option>
+                </select>
 
-                    <label>
-                        Tipo de usuario
-                    </label>
-                    <select defaultValue={"0"}>
-                        <option disabled value="0"></option>
-                        <option value="1">Administrador</option>
-                        <option value="2">Estudiante</option>
-                    </select>
+                <label>Contraseña</label>
+                <input name="password" type="password" placeholder="" onChange={handleChange} />
 
-                    <label>
-                        Contraseña
-                    </label>
-                    <input type="password" placeholder="" />
-
-                    <button type="submit">
-                        Ingresar
-                    </button>
-                    <a>
-                        Registrarse
-                    </a>
-                </form>
-            </div>
-        </>
-    )
-
+                <button type="submit">Ingresar</button>
+                <a>Registrarse</a>
+            </form>
+        </div>
+    );
 };
 
 export default FormLogin;
