dcd9a694dff30dc137bfb15df0b8580f7cbd7143
Cristian Duvan Machado Mosquera <62184928+CriistiianDM@users.noreply.github.com>
2023-06-18 01:13:00 -0500

    avance del formulario de reparacion

---
+++ src/components/utils/FormReparacion.jsx
@@ -0,0 +1,16 @@
+//import libreries
+import React from "react";
+
+
+//otra noche sin dormir :(
+const FormReparacion = () => {
+
+    return (
+        <>
+        </>
+    )
+
+}
+
+
+export default FormReparacion;
