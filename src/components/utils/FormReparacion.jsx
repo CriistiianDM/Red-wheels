639c0f7f197b0f35b936d2f5724cd4c8aa46d2d7
Cristian Duvan Machado Mosquera <62184928+CriistiianDM@users.noreply.github.com>
2023-06-18 20:59:55 -0500

    se termino el formulario de reparaciones

--- src/components/utils/FormReparacion.jsx
+++ src/components/utils/FormReparacion.jsx
@@ -3,10 +3,91 @@ import React from "react";
 
 
 //otra noche sin dormir :(
-const FormReparacion = () => {
+const FormReparacion = ({
+    handleClick,
+    src_img
+}) => {
+
+    const handleClick_ = () => {
+        handleClick? handleClick() : null;
+    }
 
     return (
         <>
+           <main className="_container_pop_rp">
+                 <div onClick={handleClick_} className="_eventoClose" />
+                <div className="_container_form_rp">
+                    <form>
+                        <div className="_container_img_">
+                            <img src={src_img} alt="img" />
+                        </div>
+                        <div className="_container_form_inpust">
+
+                            <div className="_container_form_inpust__item">
+
+                                <h1 htmlFor="name">Datos del vehiculo</h1>
+                                <div className="_all_input">
+                                    <label htmlFor="name">Marca de vehiculo</label>
+                                    <input 
+                                          placeholder="Tesla model 3" 
+                                          type="text" 
+                                          name="marca" 
+                                          id="marca" />
+                                    <label htmlFor="name">Matricula de vehiculo</label>
+                                    <input 
+                                         placeholder="IRO110"
+                                         type="text" 
+                                         name="_matricula" 
+                                         id="_matricula" />
+                                </div>
+
+                                <h1 htmlFor="name">Fecha y hora</h1>
+                                <div className="_all_input">
+                                    <input 
+                                            placeholder="Fecha"
+                                            type="date"
+                                            name="date"
+                                            id="date" />
+                                </div>
+
+                            </div>
+
+                            <div className="_container_form_inpust__item">
+                                
+                                <h1 htmlFor="name">Datos del vehiculo</h1>
+                                    <div className="_all_input">
+                                        <label htmlFor="name">Diagnostico automotriz</label>
+                                        <input
+                                            placeholder="$49.99"
+                                            type="text"
+                                            name="automotriz"
+                                            id="_automotriz" />
+                                        <label htmlFor="name">Diagnostico automotriz</label>
+                                        <input
+                                            placeholder="$49.99"
+                                            type="text"
+                                            name="automotriz"
+                                            id="_automotriz" />
+                                        <label htmlFor="name">IVA</label>
+                                        <input
+                                            placeholder="19% x MONTO TOTAL"
+                                            type="text"
+                                            name="automotriz"
+                                            id="_automotriz" />
+                                        <p>
+                                            <span>Total:</span>
+                                            <span>$$$$$$</span>
+                                        </p>
+                                    </div>
+                                </div>
+
+                        </div>
+                        <div className="_actions">
+                             <button type="submit">RESERVE SU CITA</button>
+                        </div>
+                    </form>
+                </div>
+           </main>
         </>
     )
 
