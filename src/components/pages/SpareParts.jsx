74fe02d02ec7ba23ad155712608ee936b41ac196
Brayan Sanchez <brayan.sanchez.leon@correounivalle.edu.co>
2023-06-25 20:38:22 -0500

    Add URL to get the Repuesto data based on the subsidiary

--- src/components/pages/SpareParts.jsx
+++ src/components/pages/SpareParts.jsx
@@ -1,27 +1,24 @@
 //import libraries
 import React from "react";
-import Header from '../header/Header';
-import Navbar from '../menu/Navbar';
-import Footer from '../footer/footer';
-import FilterSpareParts from '../utils/FilterSpareParts';
-import CardSpareParts from '../utils/CardSpareParts';
+import Header from "../header/Header";
+import Navbar from "../menu/Navbar";
+import Footer from "../footer/footer";
+import FilterSpareParts from "../utils/FilterSpareParts";
+import CardSpareParts from "../utils/CardSpareParts";
 
 const SpareParts = () => {
+    const [data_select, setDataSelect] = React.useState([]);
+    const [target, setTarget] = React.useState("/diagnostico-carros");
 
-    const [ data_select, setDataSelect ] = React.useState([]);
-    const [ target, setTarget ] = React.useState([]);
-
-     return (
+    return (
         <>
-          <Header />
-          <Navbar />
-          <FilterSpareParts setTarget={setTarget} setDataSelect={setDataSelect} />
-          <CardSpareParts target={target} data_respuestos={data_select} />
-          <Footer  />
+            <Header />
+            <Navbar />
+            <FilterSpareParts target={target} setTarget={setTarget} setDataSelect={setDataSelect} />
+            <CardSpareParts target={target} data_respuestos={data_select} />
+            <Footer />
         </>
-     )
-
-}
-
+    );
+};
 
 export default SpareParts;
