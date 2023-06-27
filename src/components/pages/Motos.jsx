54701f1619e83bc945d95c2754412e1358dacbdb
Brayan Sanchez <brayan.sanchez.leon@correounivalle.edu.co>
2023-06-26 20:18:49 -0500

    Delete unnecessary code

--- src/components/pages/Motos.jsx
+++ src/components/pages/Motos.jsx
@@ -4,21 +4,12 @@ import Header from "../header/Header";
 import Footer from "../footer/footer";
 import Motos from "../motos/Motos";
 import Navbar from "../menu/Navbar";
-import FilterSpareParts from "../utils/FilterSpareParts";
-// const product = {
-//    price: 100,
-// }
-
-// product.filter((item) => item.price > 100)
 
 export default function Home() {
-    const [data_select, setDataSelect] = React.useState([]);
-    const [target, setTarget] = React.useState([]);
     return (
         <>
             <Header />
             <Navbar />
-            <FilterSpareParts setTarget={setTarget} setDataSelect={setDataSelect} />
             <Motos />
             <Footer />
         </>
