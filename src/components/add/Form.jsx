887fc07914fc21d03fbc62c4e04a85b14e5c1ff2
Brayan Sanchez <brayan.sanchez.leon@correounivalle.edu.co>
2023-07-05 10:42:13 -0500

    Modify form that creates a new product

--- src/components/add/Form.jsx
+++ src/components/add/Form.jsx
@@ -4,6 +4,14 @@ import { useNavigate } from "react-router-dom";
 import { vehicle } from "../../services/product";
 
 const FormProfile = ({ product }) => {
+    const [newProduct, setNewProduct] = React.useState({
+        nombre: "",
+        marca: "",
+        descripcion: "",
+        stock: 0,
+        precio: 0,
+    });
+
     const navigate = useNavigate();
 
     const handleSubmit = async (e) => {
@@ -20,30 +28,63 @@ const FormProfile = ({ product }) => {
         }
     };
 
-    const handleChange = (e) => {};
+    const handleChange = (e) => {
+        setNewProduct({
+            ...newProduct,
+            [e.target.name]: e.target.value,
+        });
+    };
 
     return (
         <>
             <form id="Agregarcentro" onSubmit={handleSubmit}>
                 <div className="_container_form_inpust__item" id="Agrgar_producto">
                     <div className="_all_input">
-                        <label htmlFor="name">ID</label>
-                        <input placeholder="1245" type="number" name="id" id="id" />
-                        <label htmlFor="name">NOMBRE</label>
+                        <label htmlFor="nom_producto">NOMBRE</label>
                         <input
                             placeholder="Tesla Model 3"
                             type="text"
-                            name="nom_producto"
+                            name="nombre"
                             id="nom_producto"
+                            onChange={handleChange}
+                            value={newProduct.nombre}
                         />
-                        <label htmlFor="name">MARCA</label>
-                        <input placeholder="Tesla" type="text" name="marca" id="marca" />
-                        <label htmlFor="name">DESCRIPCION</label>
-                        <input placeholder="450KM" type="text" name="descrip" id="descrip" />
-                        <label htmlFor="name">CANTIDAD</label>
-                        <input placeholder="20" type="number" name="cantidad" id="cantidad" />
-                        <label htmlFor="name">PRECIO</label>
-                        <input placeholder="640000" type="text" name="precio" id="precio" />
+                        <label htmlFor="marca">MARCA</label>
+                        <input
+                            placeholder="Tesla"
+                            type="text"
+                            name="marca"
+                            id="marca"
+                            onChange={handleChange}
+                            value={newProduct.marca}
+                        />
+                        <label htmlFor="descrip">DESCRIPCION</label>
+                        <input
+                            placeholder="450KM"
+                            type="text"
+                            name="descripcion"
+                            id="descrip"
+                            onChange={handleChange}
+                            value={newProduct.descripcion}
+                        />
+                        <label htmlFor="stock">CANTIDAD</label>
+                        <input
+                            placeholder="20"
+                            type="number"
+                            name="stock"
+                            id="cantidad"
+                            onChange={handleChange}
+                            value={newProduct.stock}
+                        />
+                        <label htmlFor="precio">PRECIO</label>
+                        <input
+                            placeholder="640000"
+                            type="text"
+                            name="precio"
+                            id="precio"
+                            onChange={handleChange}
+                            value={newProduct.precio}
+                        />
                     </div>
                 </div>
                 <div className="_actions">
